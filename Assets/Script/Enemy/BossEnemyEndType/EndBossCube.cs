da7fd401c07c727e9757d3e90fd42bc55468c049
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-27 13:27:07 +0900

    1/27 자리옮기기

--- Assets/Script/Enemy/BossEnemyEndType/EndBossCube.cs
+++ Assets/Script/Enemy/BossEnemyEndType/EndBossCube.cs
@@ -7,16 +7,23 @@ public class EndBossCube : Unit
     //테스트
     [SerializeField] private bool halfPattenCheck;
     //기본스킬들 재사용 시간
-    [SerializeField]private float basicAttackTimer = 3;
+    [SerializeField] private float basicAttackTime = 2.5f;
+    private float basicAttackTimer = 3;
+    private int pattenNum = 0;
+    private bool colorChange = false;
+
     [SerializeField] private float UpGroundRange = 2;//반지름
 
+    [SerializeField]private SpriteRenderer spr;
     private bool upGroundPattening = false;
 
+    //외부참조
     private Transform playerTrs;
     
 
     void Start()
     {
+        spr = GetComponentInChildren<SpriteRenderer>();
         playerTrs = GameManager.instance.GetPlayerTransform;
     }
 
@@ -31,15 +38,37 @@ public class EndBossCube : Unit
     private void attackPatten()
     {
         basicAttackTimer += Time.deltaTime;
-        if (basicAttackTimer >= 2.5f)
+        if (basicAttackTimer >= basicAttackTime - 0.5f && colorChange == false)
         {
-            int pattenNum = Random.Range(0, 3);
+            pattenNum = Random.Range(0, 3);
+
+            if (pattenNum == 0)
+            {
+                spr.color = Color.black;
+                colorChange = true;
+            }
+            else if (pattenNum == 1)
+            {
+                spr.color = Color.red;
+                colorChange = true;
+            }
+            else if (pattenNum == 2)
+            {
+                spr.color = Color.cyan;
+                colorChange = true;
+            }
+
+        }
+        if (basicAttackTimer >= basicAttackTime)
+        {
+            
             if (pattenNum == 0)
             {
                 GameObject attackObj;
                 attackObj = PoolingManager.Instance.CreateObject(PoolingManager.ePoolingObject.LaserPatten, GameManager.instance.GetEnemyAttackObjectPatten);
                 attackObj.transform.position = transform.position;
                 basicAttackTimer = 0;
+                colorChange = false;
             }
             else if (pattenNum == 1)
             {
@@ -47,20 +76,10 @@ public class EndBossCube : Unit
                 attackObj = PoolingManager.Instance.CreateObject(PoolingManager.ePoolingObject.RedButterfly, GameManager.instance.GetEnemyAttackObjectPatten);
                 attackObj.transform.position = transform.position;
                 basicAttackTimer = 0;
+                colorChange = false;
             }
-            //else if (pattenNum == 2 && upGroundPattening == false)
-            //{
-            //    upGroundPattening = true;
-            //    StartCoroutine(upGroundPatten());
-            //}
-            //else if(pattenNum==2&& upGroundPattening == true)
-            //{
-            //    basicAttackTimer = 2.4f;
-            //}
             else if (pattenNum == 2)
             {
-
-                Debug.Log("2");
                 for (int i = 0; i < 8; i++)
                 {
                     GameObject attackObj;
@@ -75,6 +94,7 @@ public class EndBossCube : Unit
                 }
                 
                 basicAttackTimer = 0;
+                colorChange = false; 
             }
         }
     }
